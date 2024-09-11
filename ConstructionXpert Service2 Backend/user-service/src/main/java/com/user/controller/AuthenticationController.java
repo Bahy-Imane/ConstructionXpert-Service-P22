3c06575fadc3dd53ec8a91623abe3a2a7a26d154
I M A N E <imanebahy91@gmail.com>
2024-09-11 15:24:35 +0100

    add CrossOrigin To controllers

--- ConstructionXpert Service2 Backend/user-service/src/main/java/com/user/controller/AuthenticationController.java
+++ ConstructionXpert Service2 Backend/user-service/src/main/java/com/user/controller/AuthenticationController.java
@@ -14,11 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
--- ConstructionXpert-Service2-FrontEnd/src/app/component/login/login.component.ts
+++ ConstructionXpert-Service2-FrontEnd/src/app/component/login/login.component.ts
@@ -1,87 +1,87 @@
-import {Component, inject} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {AuthService} from "../../core/services/auth-service";
-import {Router} from "@angular/router";
-import {LoginUserDto} from "../../core/Dto/login-user-dto";
-import {MatCard} from "@angular/material/card";
-import {MatFormField} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
-import {MatButton} from "@angular/material/button";
-import {NgIf} from "@angular/common";
-import jwt_decode from "jwt-decode";
-
-@Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [
-    MatCard,
-    ReactiveFormsModule,
-    MatFormField,
-    MatInput,
-    MatButton,
-    NgIf
-  ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
-})
-export class LoginComponent {
-  success = false;
-  failure = false;
-  emailErrorMessage: string | null = null;
-  loginForm: FormGroup;
-
-
-  private authService = inject(AuthService);
-  private router = inject(Router);
-
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      userNameOrEmail: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
-  }
-
-  ngOnInit(): void {
-  }
-
-  login(submitData: FormGroup) {
-    this.success = false;
-    this.failure = false;
-    this.emailErrorMessage = null;
-
-    const loginUserDto: LoginUserDto = {
-      userNameOrEmail: submitData.value.userNameOrEmail,
-      password: submitData.value.password,
-    };
-
-
-    this.authService.login(loginUserDto).subscribe({
-      next: (response) => {
-        if (response.token) {
-          this.success = true;
-          console.log(response.token)
-          localStorage.setItem('accessToken', response.token);
-
-          const decodedToken: any = jwt_decode(response.token);
-          const userRole = decodedToken.role;
-
-          if (userRole === "ADMIN") {
-            this.router.navigate(['/admin-dashboard']);
-          } else if (userRole === "CUSTOMER") {
-            this.router.navigate(['/customerHome']);
-          }
-        } else {
-          this.failure = true;
-          this.emailErrorMessage = 'Login failed. Invalid response from server.';
-        }
-      },
-      error: (error) => {
-        console.error('Login error:', error);
-        this.failure = true;
-        this.emailErrorMessage = 'Login failed. Please check your credentials and try again.';
-      }
-    });
-  }
-
-
-}
+// import {Component, inject} from '@angular/core';
+// import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+// import {AuthService} from "../../core/services/auth-service";
+// import {Router} from "@angular/router";
+// import {LoginUserDto} from "../../core/Dto/login-user-dto";
+// import {MatCard} from "@angular/material/card";
+// import {MatFormField} from "@angular/material/form-field";
+// import {MatInput} from "@angular/material/input";
+// import {MatButton} from "@angular/material/button";
+// import {NgIf} from "@angular/common";
+// import jwt_decode from "jwt-decode";
+//
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [
+//     MatCard,
+//     ReactiveFormsModule,
+//     MatFormField,
+//     MatInput,
+//     MatButton,
+//     NgIf
+//   ],
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.css'
+// })
+// export class LoginComponent {
+//   success = false;
+//   failure = false;
+//   emailErrorMessage: string | null = null;
+//   loginForm: FormGroup;
+//
+//
+//   private authService = inject(AuthService);
+//   private router = inject(Router);
+//
+//   constructor(private fb: FormBuilder) {
+//     this.loginForm = this.fb.group({
+//       userNameOrEmail: ['', [Validators.required, Validators.email]],
+//       password: ['', [Validators.required, Validators.minLength(6)]]
+//     });
+//   }
+//
+//   ngOnInit(): void {
+//   }
+//
+//   login(submitData: FormGroup) {
+//     this.success = false;
+//     this.failure = false;
+//     this.emailErrorMessage = null;
+//
+//     const loginUserDto: LoginUserDto = {
+//       userNameOrEmail: submitData.value.userNameOrEmail,
+//       password: submitData.value.password,
+//     };
+//
+//
+//     this.authService.login(loginUserDto).subscribe({
+//       next: (response) => {
+//         if (response.token) {
+//           this.success = true;
+//           console.log(response.token)
+//           localStorage.setItem('accessToken', response.token);
+//
+//           const decodedToken: any = jwt_decode(response.token);
+//           const userRole = decodedToken.role;
+//
+//           if (userRole === "ADMIN") {
+//             this.router.navigate(['/admin-dashboard']);
+//           } else if (userRole === "CUSTOMER") {
+//             this.router.navigate(['/customerHome']);
+//           }
+//         } else {
+//           this.failure = true;
+//           this.emailErrorMessage = 'Login failed. Invalid response from server.';
+//         }
+//       },
+//       error: (error) => {
+//         console.error('Login error:', error);
+//         this.failure = true;
+//         this.emailErrorMessage = 'Login failed. Please check your credentials and try again.';
+//       }
+//     });
+//   }
+//
+//
+// }
